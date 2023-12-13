18f50a94105e2dca64e0a5f30db26384a94e8f88
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2023-12-13 08:47:08 -0800

    refactor(wezterm): factor out color scheme to separate file

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,41 +10,16 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
--- config.color_scheme = "Catppuccin Macchiato"
+-- color config
+config.color_scheme = "Dark One Nuanced Custom"
+
+-- font config
 -- config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Bold", italic = false })
-config.font = wezterm.font_with_fallback {
+config.font = wezterm.font_with_fallback({
 	{ family = "JetBrains Mono", weight = "Bold", italic = false },
 	"Apple Color Emoji",
-}
+})
 config.font_size = 14.0
-
--- 'Dark One Nuanced' theme ported from kitty theme
-config.colors = {
-	ansi = {
-		"#3f4451",
-		"#ed8796",
-		"#a6da95",
-		"#d19a66",
-		"#61afef",
-		"#c678dd",
-		"#56b6c2",
-		"#e6e6e6",
-	},
-	brights = {
-		"#4f5666",
-		"#ff7b86",
-		"#b1e18b",
-		"#efb074",
-		"#67cdff",
-		"#e48bff",
-		"#63d4e0",
-		"#ffffff",
-	},
-	background = "#282c34",
-	foreground = "#abb2bf",
-	cursor_bg = '#a6da95'
-}
 
 -- tab config
 config.hide_tab_bar_if_only_one_tab = true
@@ -56,7 +31,7 @@ config.window_decorations = "RESIZE"
 
 -- cursor config
 config.cursor_blink_rate = 750
-config.default_cursor_style = 'BlinkingBlock'
+config.default_cursor_style = "BlinkingBlock"
 
 -- keybindings
 config.keys = {
