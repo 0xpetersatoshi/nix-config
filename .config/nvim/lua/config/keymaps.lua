2027809d2176a13e979ee499edba870a21b82e5d
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2024-08-08 22:05:35 -0700

    refactor(nvim): update debugger settings and update all lazy plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,9 +9,9 @@ vim.api.nvim_set_keymap("n", "<M-S-j>", ":resize -2<CR>", { noremap = true, sile
 vim.api.nvim_set_keymap("n", "<M-S-k>", ":resize +2<CR>", { noremap = true, silent = true })
 
 -- Map Option+Shift+J to decrease window height
-vim.api.nvim_set_keymap("n", "<M-S-h>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-S-h>", ":vertical resize +2<CR>", { noremap = true, silent = true })
 
 -- Map Option+Shift+K to increase window height
-vim.api.nvim_set_keymap("n", "<M-S-l>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-S-l>", ":vertical resize -2<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
