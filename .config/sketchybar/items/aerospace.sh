d1710a06df2dc9f1f271edb8ea1037ab62c6c7ee
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-06 13:49:41 -0800

    refactor(aerospace): configure aerospace to work with sketchybar

--- .config/sketchybar/items/aerospace.sh
+++ .config/sketchybar/items/aerospace.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 sketchybar --add event aerospace_workspace_change
-for sid in $(aerospace list-workspaces --focused); do
-  sketchybar --add item space."$sid" left \
-    --subscribe space."$sid" aerospace_workspace_change \
-    --set space."$sid" \
-    background.drawing=off \
-    label.font.size=14.0 \
-    label="$sid" \
-    click_script="aerospace workspace $sid" \
-    script="$PLUGIN_DIR/aerospace.sh $sid"
-done
+sid = $(aerospace list-workspaces --focused)
+sketchybar --add item space.aerospace left \
+  --subscribe space.aerospace aerospace_workspace_change \
+  --set space.aerospace \
+  background.drawing=off \
+  label.font.size=14.0 \
+  label="$sid" \
+  script="$PLUGIN_DIR/aerospace.sh"
--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-echo "called with $1"
-echo "$FOCUSED_WORKSPACE"
-
 if [ "$SENDER" = "aerospace_workspace_change" ]; then
-  echo "INFO: $INFO"
-  sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME label=$FOCUSED_WORKSPACE background.drawing=on
 fi
