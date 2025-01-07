03e59d05cce848489af47898e38ca61ec9163d27
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-07 11:16:59 -0800

    refactor(aerospace): update aerospace config and appearance in sketchybar

--- .config/sketchybar/items/aerospace.sh
+++ .config/sketchybar/items/aerospace.sh
@@ -4,7 +4,11 @@ sketchybar --add event aerospace_workspace_change
 sketchybar --add item space.aerospace left \
   --subscribe space.aerospace aerospace_workspace_change \
   --set space.aerospace \
-  background.drawing=off \
+  background.drawing=on \
   label.font.size=14.0 \
+  icon.padding_left=10 \
+  icon.padding_right=0 \
+  label.padding_left=0 \
+  label.padding_right=10 \
   label="$(aerospace list-workspaces --focused)" \
   script="$PLUGIN_DIR/aerospace.sh"
--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 if [ "$SENDER" = "aerospace_workspace_change" ]; then
-  sketchybar --set $NAME label=$FOCUSED_WORKSPACE background.drawing=on
+  sketchybar --set $NAME label=$FOCUSED_WORKSPACE
 fi
