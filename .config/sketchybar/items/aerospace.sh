8465c5a8229ac2d2da4a5aa463057f80aa7bb8aa
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-06 20:07:52 -0800

    chore(aerospace): define per-monitor gap values

--- .config/sketchybar/items/aerospace.sh
+++ .config/sketchybar/items/aerospace.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 sketchybar --add event aerospace_workspace_change
-sid = $(aerospace list-workspaces --focused)
 sketchybar --add item space.aerospace left \
   --subscribe space.aerospace aerospace_workspace_change \
   --set space.aerospace \
   background.drawing=off \
   label.font.size=14.0 \
-  label="$sid" \
+  label="$(aerospace list-workspaces --focused)" \
   script="$PLUGIN_DIR/aerospace.sh"
