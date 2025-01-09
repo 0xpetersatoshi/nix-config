053982f32b65ecf5fcf47ec88eb2db7b746eb270
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-08 20:48:18 -0800

    refactor(sketchybar): update media plugin

--- .config/sketchybar/items/media.sh
+++ .config/sketchybar/items/media.sh
@@ -3,10 +3,10 @@
 sketchybar --add item media e \
   --set media label.color=$ACCENT_COLOR \
   label.max_chars=20 \
-  icon.padding_left=5 \
   scroll_texts=on \
   icon=􀑪 \
   icon.color=$ACCENT_COLOR \
-  background.drawing=off \
+  background.drawing=on \
+  background.padding_left=15 \
   script="$PLUGIN_DIR/media.sh" \
   --subscribe media media_change
--- .config/sketchybar/plugins/media.sh
+++ .config/sketchybar/plugins/media.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env sh
 
 STATE="$(echo "$INFO" | jq -r '.state')"
+APP="$(echo "$INFO" | jq -r '.app')"
+
+case $APP in
+"Music")
+  ICON="􀒷"
+  ICON_COLOR="0xfffc3c44"
+  ;;
+"Spotify")
+  ICON=""
+  ICON_COLOR="0xff1db954"
+  ;;
+esac
+
 if [ "$STATE" = "playing" ]; then
   MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  sketchybar --set $NAME label="$MEDIA" drawing=on
+  sketchybar --set $NAME label="$MEDIA" drawing=on icon="$ICON" icon.color="$ICON_COLOR"
 else
   sketchybar --set $NAME drawing=off
 fi
