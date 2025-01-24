6e88b095def7f666aa84b9d3b9a2ad77500e25bb
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-23 23:32:20 -0800

    feat(flake): add initial sample flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: rec {
+      # System Settings
+      systemSettings = {
+          system = "aarch64-darwin";
+          hostname = "nova";
+      };
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+          pkgs.nushell
+        ];
+
+      security.pam.enableSudoTouchIdAuth = true;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = systemSettings.system;
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations.systemSettings.hostname = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
