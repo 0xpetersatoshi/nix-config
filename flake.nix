c9c36769d52e95dd9754cb1aff203b68acd90778
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-24 20:42:04 -0800

    refactor(flake): move settings into module

--- flake.nix
+++ flake.nix
@@ -15,41 +15,19 @@
         system = "aarch64-darwin";
         hostname = "nova";
     };
-
-    configuration = { pkgs, systemSettings, ... }: rec {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.nushell
-          pkgs.direnv
-        ];
-
-      security.pam.enableSudoTouchIdAuth = true;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "${systemSettings.system}";
-    };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."${systemSettings.hostname}" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-      specialArgs = { inherit systemSettings; };
+      system = systemSettings.system;
+      modules = [
+          ./hosts/${systemSettings.hostname}/default.nix
+      ];
+      specialArgs = {
+          inherit inputs;
+          inherit systemSettings;
+      };
     };
   };
 }
