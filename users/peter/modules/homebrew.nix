ebce75ca52630da4aad05824544553629da3570d
0xPeterSatoshi <dev@ngml.me>
2025-01-26 14:10:48 -0800

    feat(nix): add homebrew configuration but don't enable

---
+++ users/peter/modules/homebrew.nix
@@ -0,0 +1,16 @@
+{
+    homebrew = {
+    enable = false;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+      upgrade = true;
+    };
+    brewPrefix = "/opt/homebrew/bin";
+    caskArgs = {
+      no_quarantine = true;
+    };
+    casks = [
+    ];
+  };
+}
