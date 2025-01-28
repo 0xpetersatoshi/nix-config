0fd361f3ce336b5ffffb51c7101501f5aa0e9638
0xPeterSatoshi <dev@ngml.me>
2025-01-28 02:10:49 -0800

    refactor(modules): update styles module

--- modules/home/styles/stylix/default.nix
+++ modules/home/styles/stylix/default.nix
@@ -10,16 +10,49 @@ in {
   imports = with inputs; [
     stylix.homeManagerModules.stylix
   ];
-
   options.styles.stylix = {
     enable = lib.mkEnableOption "Enable stylix";
   };
 
   config = lib.mkIf cfg.enable {
-
     stylix = {
       enable = true;
       autoEnable = true;
+      polarity = "dark";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+      # iconTheme = {
+      #   enable = true;
+      #   package = pkgs.catppuccin-papirus-folders.override {
+      #     flavor = "mocha";
+      #     accent = "lavender";
+      #   };
+      #   dark = "Papirus-Dark";
+      # };
+
+      fonts = {
+        emoji = {
+          name = "Noto Color Emoji";
+          package = pkgs.noto-fonts-color-emoji;
+        };
+        monospace = {
+          name = "JetBrains Mono";
+          package = pkgs.jetbrains-mono;
+        };
+        sansSerif = {
+          name = "Noto Sans";
+          package = pkgs.noto-fonts;
+        };
+        serif = {
+          name = "Noto Serif";
+          package = pkgs.noto-fonts;
+        };
+
+        sizes = {
+          terminal = 13;
+          applications = 12;
+        };
+      };
 
       image = pkgs.fetchurl {
         url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/media/branch/master/png/gruvbox-dark-blue.png";
