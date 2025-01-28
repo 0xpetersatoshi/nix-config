437a1ca35197f24b9e50ba7383829cec1cf64d71
0xPeterSatoshi <dev@ngml.me>
2025-01-27 23:03:44 -0800

    refactor(modules): move zsh and nushell to modules directory

---
+++ modules/home/cli/shells/zsh/default.nix
@@ -0,0 +1,81 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.igloo; let
+  cfg = config.cli.shells.zsh;
+
+  # TODO: Define this in a common module
+  shellAliases = {
+    cat = "bat";
+    ll = "eza --icons=always -l";
+    tree = "eza --icons=always --tree";
+    v = "nvim";
+  };
+
+  homebrewInitExtra = ''
+    # Setup the brew package manager for GUI apps
+    if [[ -f "/opt/homebrew/bin/brew" ]] then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+  '';
+in {
+  options.cli.shells.zsh = with types; {
+    enable = mkBoolOpt false "enable zsh shell";
+  };
+
+  config = mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+
+      initExtra = ''
+        ${
+          if pkgs.stdenv.isDarwin
+          then homebrewInitExtra
+          else ""
+        }
+
+        # Completion styling
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+        zstyle ':completion:*' menu no
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+        # Kitty key bindings
+        bindkey '\e[H' beginning-of-line
+        bindkey '\e[F' end-of-line
+      '';
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "aws"
+          "command-not-found"
+          "git"
+          "kubectl"
+          "sudo"
+        ];
+      };
+      plugins = [
+        {
+          name = "fzf-tab";
+          src = pkgs.fetchFromGitHub {
+            owner = "Aloxaf";
+            repo = "fzf-tab";
+            rev = "v1.1.2";
+            sha256 = "Qv8zAiMtrr67CbLRrFjGaPzFZcOiMVEFLg1Z+N6VMhg=";
+          };
+        }
+      ];
+
+      inherit shellAliases;
+    };
+  };
+}
