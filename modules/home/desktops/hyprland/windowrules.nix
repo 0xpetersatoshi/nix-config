dadb52bfe66ae886677cb6ef988992fa803c4216
0xPeterSatoshi <dev@ngml.me>
2025-02-08 00:15:13 -0800

    chore(hyprland): add window rules to hide xwaylandvideobridge

--- modules/home/desktops/hyprland/windowrules.nix
+++ modules/home/desktops/hyprland/windowrules.nix
@@ -14,6 +14,14 @@ in {
 
       windowrulev2 = [
         "idleinhibit fullscreen, class:^(firefox)$"
+
+        # hide xwaylandvideobridge
+        "opacity 0.0 override, class:^(xwaylandvideobridge)$"
+        "noanim, class:^(xwaylandvideobridge)$"
+        "noinitialfocus, class:^(xwaylandvideobridge)$"
+        "maxsize 1 1, class:^(xwaylandvideobridge)$"
+        "noblur, class:^(xwaylandvideobridge)$"
+        "nofocus, class:^(xwaylandvideobridge)$"
       ];
     };
   };
