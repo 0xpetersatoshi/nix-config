d8d4995d77bf52f073476a9e5908587977333550
0xPeterSatoshi <dev@ngml.me>
2025-02-08 16:44:35 -0800

    feat(modules): add kde addon module

--- homes/x86_64-linux/peter@nixbox/default.nix
+++ homes/x86_64-linux/peter@nixbox/default.nix
@@ -7,20 +7,18 @@
         "${pkgs.blueman}/bin/blueman-applet"
       ];
     };
+
+    addons.kde.enable = true;
   };
 
   home.packages = with pkgs; [
     nwg-displays
     hyprpolkitagent
-    libsForQt5.dolphin
-    libsForQt5.dolphin-plugins
-    libsForQt5.kwallet
-    libsForQt5.kwalletmanager
     libsForQt5.xwaylandvideobridge
     libnotify
     xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
-  ];
+    ];
 
   cli.programs.git.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFjoHku2U1i34uJWA6kODHU44QJCpQE7LHxYQgk382h";
 
---
+++ modules/home/desktops/addons/kde/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.desktops.addons.kde;
+in {
+  options.desktops.addons.kde = {
+    enable = mkEnableOption "enable kde addons";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      # Qt6/KDE Components
+      ark # KDE file archiver
+      kdePackages.dolphin # KDE file manager
+      kdePackages.qtimageformats # Additional image format plugins for Qt6
+      kdePackages.ffmpegthumbs # FFmpeg-based video thumbnails for Qt6
+      kdePackages.kde-cli-tools # KDE command line tools
+      kdePackages.kdegraphics-thumbnailers # KDE graphics file thumbnails
+      kdePackages.kimageformats # Additional image format plugins for KDE
+      kdePackages.qtimageformats # imageformats for Qt6
+      kdePackages.qtwayland # Qt6 Wayland integration
+      kdePackages.qtsvg # SVG support for Qt6
+      kdePackages.qtbase # Qt6 base libraries
+      kdePackages.kio # KDE Input/Output framework
+      kdePackages.kio-extras # Additional KIO protocols and file systems
+      kdePackages.kwayland # KDE Wayland integration library
+      kdePackages.qt5compat # Qt5 compatibility layer for Qt6
+      kdePackages.qtstyleplugin-kvantum # Qt6 Kvantum style plugin
+      kdePackages.qt6ct # Qt6 configuration tool
+      kdePackages.qt6gtk2 # GTK2-style widgets for Qt6
+    ];
+  };
+}
