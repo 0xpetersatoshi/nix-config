d4d929793a4ec760494d10721dc74bac28d3478c
0xPeterSatoshi <dev@ngml.me>
2025-02-09 20:35:20 -0800

    feat: add sugar candy sddm theme and enable it for sddm module

--- modules/nixos/roles/desktop/addons/kde/default.nix
+++ modules/nixos/roles/desktop/addons/kde/default.nix
@@ -8,12 +8,17 @@
 with lib;
 with lib.${namespace}; let
   cfg = config.roles.desktop.addons.kde;
+  sddm-candy = pkgs.callPackage ../../../../../../themes/sddm/candy.nix {};
 in {
   options.roles.desktop.addons.kde = with types; {
     enable = mkBoolOpt false "Enable or disable KDE components";
   };
 
   config = mkIf cfg.enable {
+    environment.pathsToLink = [
+      "/share/themes"
+    ];
+
     services = {
       displayManager = {
         sddm = {
@@ -31,17 +36,15 @@ in {
             libsForQt5.qtsvg # for sddm theme svg icons
             libsForQt5.qt5.qtwayland # wayland support for qt5
           ];
-          # TODO: use sddm sugar candy theme
-          # https://framagit.org/MarianArlt/sddm-sugar-candy
-          theme = "";
+          theme = "Candy";
           settings = {
             General = {
               GreeterEnvironment = "QT_WAYLAND_SHELL_INTEGRATION=layer-shell";
             };
-            # Theme = {
-            #   ThemeDir = "/run/current-system/sw/share/sddm/themes";
-            #   CursorTheme = "Bibata-Modern-Ice";
-            # };
+            Theme = {
+              ThemeDir = "/run/current-system/sw/share/sddm/themes";
+              # CursorTheme = "Bibata-Modern-Ice";
+            };
           };
         };
         sessionPackages = [pkgs.hyprland];
@@ -55,11 +58,23 @@ in {
       };
     };
 
-    qt = {
-      enable = true;
-      platformTheme = "qt5ct";
-      style = "kvantum";
-    };
+    environment.systemPackages = with pkgs; [
+      sddm-candy
+      libsForQt5.qt5.qtquickcontrols # for sddm theme ui elements
+      libsForQt5.layer-shell-qt # for sddm theme wayland support
+      libsForQt5.qt5.qtquickcontrols2 # for sddm theme ui elements
+      libsForQt5.qt5.qtgraphicaleffects # for sddm theme effects
+      libsForQt5.qtsvg # for sddm theme svg icons
+      libsForQt5.qt5.qtwayland # wayland support for qt5
+
+      polkit_gnome # polkit gui
+    ];
+
+    # qt = {
+    #   enable = true;
+    #   platformTheme = "qt5ct";
+    #   style = "kvantum";
+    # };
 
     programs.dconf.enable = true;
   };
