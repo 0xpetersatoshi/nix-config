ddaa4d6d86b1a9b0f28caf4f826208f2f6a8a3fa
0xPeterSatoshi <dev@ngml.me>
2025-02-12 23:22:34 -0800

    feat(nvim): add neovim module

---
+++ modules/home/cli/editors/neovim/default.nix
@@ -0,0 +1,49 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.igloo; let
+  cfg = config.cli.editors.neovim;
+in {
+  options.cli.editors.neovim = with types; {
+    enable = mkBoolOpt false "enable zellij multiplexer";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      alejandra
+      bash-language-server
+      docker-compose-language-service
+      gcc
+      # TODO: need to install these manually for now:
+      # https://github.com/0no-co/graphqlsp
+      # https://github.com/0no-co/gql.tada
+      hadolint
+      lua-language-server
+      markdownlint-cli2
+      marksman
+      nixd
+      shfmt
+      # TODO: need to find a solution for this as its not available in nixpkgs
+      # npm install @nomicfoundation/solidity-language-server -g
+      sqls
+      stylua
+      taplo
+      terraform-ls
+      vim-language-server
+      # For setting up rust DAP
+      vscode-extensions.vadimcn.vscode-lldb
+      # Includes vscode-json-language-server
+      vscode-langservers-extracted
+      yaml-language-server
+    ];
+
+    xdg.configFile.nvim = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/nvim";
+      recursive = true;
+    };
+  };
+}
