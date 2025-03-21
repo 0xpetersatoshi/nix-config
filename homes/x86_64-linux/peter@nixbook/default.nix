bba050a9a963b200bb43db7568ff109833921d9b
0xPeterSatoshi <dev@ngml.me>
2025-03-21 00:53:32 -0700

    feat(homes): add nixbook home

---
+++ homes/x86_64-linux/peter@nixbook/default.nix
@@ -0,0 +1,35 @@
+{pkgs, ...}: {
+  desktops = {
+    hyprland = {
+      enable = true;
+    };
+
+    addons.kde.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    nwg-displays
+    hyprpolkitagent
+    kdePackages.xwaylandvideobridge
+    libnotify
+    nix-prefetch
+    nix-prefetch-scripts
+  ];
+
+  cli.programs.git.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFjoHku2U1i34uJWA6kODHU44QJCpQE7LHxYQgk382h";
+
+  roles = {
+    common.enable = true;
+    desktop.enable = true;
+    development.enable = true;
+  };
+
+  igloo.user = {
+    enable = true;
+    name = "peter";
+  };
+
+  styles.stylix.wallpaperPath = ../../../wallpaper/standard/astronaut-3-2912x1632.png;
+
+  home.stateVersion = "24.11";
+}
