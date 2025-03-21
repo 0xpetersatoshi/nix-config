ddfe4f5ebb5da1f18ed06e538cc9ae0435c43d12
0xPeterSatoshi <dev@ngml.me>
2025-03-21 00:52:14 -0700

    feat(nixbook): add laptop config options

--- systems/x86_64-linux/nixbook/default.nix
+++ systems/x86_64-linux/nixbook/default.nix
@@ -13,7 +13,11 @@
     virtualisation = {
       podman.enable = true;
     };
+    thermald.enable = true;
+    tlp.enable = true;
   };
+
+  powerManagement.enable = true;
 
   roles = {
     desktop = {
@@ -31,6 +35,10 @@
     # supportedFilesystems = lib.mkForce ["btrfs"];
     kernelPackages = pkgs.unstable.linuxPackages_zen;
     # resumeDevice = "/dev/disk/by-label/nixos";
+
+    # TODO: add iGPU device id
+    kernelParams = ["i915.force_probe=<device ID>"];
+    # kernelParams = ["intel_idle.max_cstate=1"];
   };
 
   hardware = {
@@ -39,6 +47,8 @@
       hasIntelCpu = true;
     };
   };
+
+  styles.stylix.wallpaperPath = ../../../wallpaper/standard/astronaut-3-2912x1632.png;
 
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "wayland";
