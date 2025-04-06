f22a4bdcd7c8b2b3ec1e599957a3c9c1df2aec04
0xPeterSatoshi <dev@ngml.me>
2025-04-06 15:02:51 -0700

    chore(flake): update hyprpanel flake

--- flake.nix
+++ flake.nix
@@ -53,7 +53,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    hyprpanel = {
+      url = "github:Jas-SinghFSU/HyprPanel";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
