d5f55b7027fceccb9d7c893f6b8c680b370a0ad2
0xPeterSatoshi <dev@ngml.me>
2025-04-15 17:40:51 -0700

    Revert "refactor(disko): update disko file to only define partitions"
    This reverts commit 3bcf23028a3d94272bd64e58e12d99f020226f6a.

--- systems/x86_64-linux/nixbook/disks.nix
+++ systems/x86_64-linux/nixbook/disks.nix
@@ -1,80 +1,89 @@
 {
   disko.devices = {
-    partition = {
-      nixos = {
-        start = "502G";
-        end = "+1T";
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-Corsair_MP600_MICRO_AA4GB506003BK9";
         content = {
-          type = "luks";
-          name = "nixos-root";
-          # disable settings.keyFile if you want to use interactive password entry
-          #passwordFile = "/tmp/secret.key"; # Interactive
-          settings = {
-            allowDiscards = true;
-            # disable to use interactive password entry
-            keyFile = "/tmp/secret.key";
-          };
-          # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
-          content = {
-            type = "btrfs";
-            extraArgs = ["-L" "nixos" "-f"];
-            subvolumes = {
-              "/root" = {
-                mountpoint = "/";
-                mountOptions = ["compress=zstd" "noatime"];
-              };
-              "/home" = {
-                mountpoint = "/home";
-                mountOptions = ["compress=zstd" "noatime"];
-              };
-              "/nix" = {
-                mountpoint = "/nix";
-                mountOptions = ["compress=zstd" "noatime"];
-              };
-              "/swap" = {
-                mountpoint = "/.swapvol";
-                mountOptions = ["noatime"];
-                swap.swapfile.size = "1G";
+          type = "gpt";
+          partitions = {
+            nixos = {
+              start = "502G";
+              end = "+1T";
+              content = {
+                type = "luks";
+                name = "nixos-root";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                  # disable to use interactive password entry
+                  keyFile = "/tmp/secret.key";
+                };
+                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-L" "nixos" "-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      mountOptions = ["noatime"];
+                      swap.swapfile.size = "1G";
+                    };
+                  };
+                };
               };
             };
-          };
-        };
-      };
 
-      archlinux = {
-        start = "after:nixos";
-        end = "100%";
-        content = {
-          type = "luks";
-          name = "archlinux-root";
-          # disable settings.keyFile if you want to use interactive password entry
-          #passwordFile = "/tmp/secret.key"; # Interactive
-          settings = {
-            allowDiscards = true;
-            # disable to use interactive password entry
-            keyFile = "/tmp/secret.key";
-          };
-          # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
-          content = {
-            type = "btrfs";
-            extraArgs = ["-L" "archlinux" "-f"];
-            subvolumes = {
-              "/root" = {
-                mountpoint = "/";
-                mountOptions = ["compress=zstd" "noatime"];
-              };
-              "/home" = {
-                mountpoint = "/home";
-                mountOptions = ["compress=zstd" "noatime"];
-              };
-              "/nix" = {
-                mountpoint = "/nix";
-                mountOptions = ["compress=zstd" "noatime"];
-              };
-              "/swap" = {
-                mountpoint = "/.swapvol";
-                mountOptions = ["noatime"];
-                swap.swapfile.size = "1G";
+            archlinux = {
+              start = "after:nixos";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "archlinux-root";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                  # disable to use interactive password entry
+                  keyFile = "/tmp/secret.key";
+                };
+                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-L" "archlinux" "-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      mountOptions = ["noatime"];
+                      swap.swapfile.size = "1G";
+                    };
+                  };
+                };
               };
             };
           };
