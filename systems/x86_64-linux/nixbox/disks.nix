4581e3bc2042e947b19e2477bf49fe102643884c
0xPeterSatoshi <dev@ngml.me>
2025-04-20 18:48:41 -0700

    refactor(nixbox): update disko configuration

--- systems/x86_64-linux/nixbox/disks.nix
+++ systems/x86_64-linux/nixbox/disks.nix
@@ -7,8 +7,19 @@
         content = {
           type = "gpt";
           partitions = {
+            ESP = {
+              size = "2G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+
             nixos = {
-              size = "1500G";
+              size = "100%";
               content = {
                 type = "luks";
                 name = "nixos-root";
@@ -23,45 +34,6 @@
                 content = {
                   type = "btrfs";
                   extraArgs = ["-L" "nixos" "-f"];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/swap" = {
-                      mountpoint = "/.swapvol";
-                      mountOptions = ["noatime"];
-                      swap.swapfile.size = "1G";
-                    };
-                  };
-                };
-              };
-            };
-
-            archlinux = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "archlinux-root";
-                # disable settings.keyFile if you want to use interactive password entry
-                #passwordFile = "/tmp/secret.key"; # Interactive
-                settings = {
-                  allowDiscards = true;
-                  # disable to use interactive password entry
-                  keyFile = "/tmp/secret.key";
-                };
-                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
-                content = {
-                  type = "btrfs";
-                  extraArgs = ["-L" "archlinux" "-f"];
                   subvolumes = {
                     "/root" = {
                       mountpoint = "/";
