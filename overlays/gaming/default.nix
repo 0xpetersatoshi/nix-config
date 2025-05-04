971a4c22537d511fb570d94752d6132469eb27a4
0xPeterSatoshi <dev@ngml.me>
2025-05-03 20:51:39 -0700

    chore(overlays): use stable version of steam

--- overlays/gaming/default.nix
+++ overlays/gaming/default.nix
@@ -2,6 +2,4 @@
   # Use unstable versions for gaming-related packages
   mangohud = channels.nixpkgs-unstable.mangohud;
   gamescope = channels.nixpkgs-unstable.gamescope;
-  steam = channels.nixpkgs-unstable.steam;
-  steam-run = channels.nixpkgs-unstable.steam-run;
 }
