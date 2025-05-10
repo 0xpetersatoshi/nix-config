f2d97a9e359d118fe0e2f0ba3a83c96bce1d81f0
0xPeterSatoshi <dev@ngml.me>
2025-05-09 19:28:48 -0700

    chore(zenbook16-370): add new host configuration

---
+++ homes/x86_64-linux/peter@zenbook16-370/default.nix
@@ -0,0 +1,44 @@
+{pkgs, ...}: {
+  desktops = {
+    hyprland = {
+      enable = true;
+      monitor = "eDP-1, highrr, auto, 1.25";
+      drmDevices = "/dev/dri/card1;/dev/dri/card0";
+      execOnceExtras = [
+        "${pkgs.libinput-gestures}/bin/libinput-gestures &"
+      ];
+    };
+
+    addons = {
+      hyprpanel = {
+        wallpaperPath = ../../../wallpaper/standard/astronaut-5-2912x1632.jpg;
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    nwg-displays
+    hyprpolkitagent
+    kdePackages.xwaylandvideobridge
+    libnotify
+    nix-prefetch
+    nix-prefetch-scripts
+  ];
+
+  cli.programs.git.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFjoHku2U1i34uJWA6kODHU44QJCpQE7LHxYQgk382h";
+
+  guis.browsers.msedge.enable = true;
+
+  roles = {
+    common.enable = true;
+    desktop.enable = true;
+    development.enable = true;
+  };
+
+  igloo.user = {
+    enable = true;
+    name = "peter";
+  };
+
+  home.stateVersion = "24.11";
+}
