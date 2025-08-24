6b7fa135db573f263b9aa89e6cc6a144c993d5cb
0xPeterSatoshi <dev@ngml.me>
2025-08-24 09:22:32 -0700

    feat(smb): add media-archive smb

--- modules/nixos/services/samba/default.nix
+++ modules/nixos/services/samba/default.nix
@@ -38,5 +38,22 @@ in {
         "dir_mode=0775"
       ];
     };
+
+    fileSystems."/mnt/media-archive" = {
+      device = "//10.19.50.2/media-archive";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in [
+        # HACK: gpg with yubikey <> sops-nix configuration not working as expected with nixos module
+        # will need to manually create secret file for now until a fix is found
+        "${automount_opts},credentials=${config.users.users.${user.name}.home}/.config/samba-secrets"
+        "uid=${toString config.users.users.${user.name}.uid}"
+        "gid=${toString config.users.groups.${group}.gid}"
+        "file_mode=0664"
+        "dir_mode=0775"
+      ];
+    };
   };
 }
