47c381b6b1af8c77e0d922401e99d2de35dcaf1a
0xPeterSatoshi <dev@ngml.me>
2025-10-09 10:13:50 -0700

    feat(nvim): define rust lsp and add codelldb debugger

--- modules/home/cli/languages/rust/default.nix
+++ modules/home/cli/languages/rust/default.nix
@@ -16,6 +16,8 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       rustup
+      # codelldb debugger
+      vscode-extensions.vadimcn.vscode-lldb
     ];
 
     home.file.".cargo/config.toml".text = ''
