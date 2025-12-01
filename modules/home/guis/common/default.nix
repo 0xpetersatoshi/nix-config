bcf49d8fe2eab1fb2e3945cfdcf2b515a479147f
0xPeterSatoshi <dev@ngml.me>
2025-11-30 21:29:11 -0700

    feat(cider): add cider music player app

--- modules/home/guis/common/default.nix
+++ modules/home/guis/common/default.nix
@@ -9,6 +9,20 @@ with lib;
 with lib.${namespace};
 with types; let
   cfg = config.guis.common;
+
+  # HACK: electron apps in hyprland need special flags for proper Wayland support
+  # and password storage configuration. Cider is an Electron app that needs:
+  # 1. Ozone platform flags for native Wayland rendering
+  # 2. Password store flag to work with kwallet6 on Hyprland
+  cider-wrapped = pkgs.symlinkJoin {
+    name = "cider";
+    paths = [pkgs.cider];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/cider \
+        --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland --password-store=kwallet6"
+    '';
+  };
 in {
   options.guis.common = {
     enable = mkEnableOption "Enable common guis";
@@ -17,6 +31,7 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       cava # audio visualizer
+      cider-wrapped # apple music player
       foliate
       keymapp
       pavucontrol
