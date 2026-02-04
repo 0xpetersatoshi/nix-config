7cabb2c56bb350d1dc91b259e9528f650642e89b
0xPeterSatoshi <dev@ngml.me>
2026-02-04 14:27:00 -0800

    refactor(nvim): add formatting, linting, and testing support for solidity

--- nvim/lua/plugins/lang/solidity.lua
+++ nvim/lua/plugins/lang/solidity.lua
@@ -1,4 +1,5 @@
 return {
+  -- LSP
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -6,8 +7,45 @@ return {
         solidity = {
           cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
           filetypes = { "solidity" },
+          root_markers = { "foundry.toml", "hardhat.config.js", "hardhat.config.ts", ".git" },
           single_file_support = true,
         },
+      },
+    },
+  },
+
+  -- Formatting
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        solidity = { "forge_fmt" },
+      },
+    },
+  },
+
+  -- Linting
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        solidity = { "solhint" },
+      },
+    },
+  },
+
+  -- Testing
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "llllvvuu/neotest-foundry",
+    },
+    opts = {
+      adapters = {
+        ["neotest-foundry"] = {},
       },
     },
   },
