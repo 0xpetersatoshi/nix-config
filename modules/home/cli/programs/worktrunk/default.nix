9270bc76e928650c6a5b1db1c30b7aa1f7abaece
0xPeterSatoshi <dev@ngml.me>
2026-03-03 14:23:19 -0800

    refactor(worktrunk): configure shell integration

--- modules/home/cli/programs/worktrunk/default.nix
+++ modules/home/cli/programs/worktrunk/default.nix
@@ -9,14 +9,23 @@
 with lib;
 with lib.${namespace}; let
   cfg = config.cli.programs.worktrunk;
+  wtPkg = inputs.worktrunk.packages."${pkgs.system}".default;
 in {
   options.cli.programs.worktrunk = with types; {
     enable = mkBoolOpt false "Whether or not to enable the worktrunk (wt) CLI";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [
-      inputs.worktrunk.packages."${pkgs.system}".default
-    ];
+    home.packages = [wtPkg];
+
+    programs.zsh.initContent = mkIf config.programs.zsh.enable ''
+      eval "$(command ${wtPkg}/bin/wt config shell init zsh)"
+    '';
+
+    programs.nushell.extraConfig = mkIf config.programs.nushell.enable (
+      builtins.readFile (pkgs.runCommand "wt-nushell-init" {} ''
+        ${wtPkg}/bin/wt config shell init nu > $out
+      '')
+    );
   };
 }
