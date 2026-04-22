fd19e4beaee286453ab02d45dd772143cd6e09e7
0xPeterSatoshi <dev@ngml.me>
2026-04-22 11:54:14 -0700

    feat(openvpn): configure openvpn

--- modules/nixos/services/vpn/openvpn/default.nix
+++ modules/nixos/services/vpn/openvpn/default.nix
@@ -8,14 +8,36 @@
 with lib;
 with lib.${namespace}; let
   cfg = config.services.vpn.openvpn;
+  updateResolved = pkgs.update-systemd-resolved;
 in {
   options.services.vpn.openvpn = with types; {
     enable = mkBoolOpt false "Whether or not to enable openvpn.";
+
+    name = mkOpt str "client" "Name for the OpenVPN connection (used in systemd service name).";
+
+    configFile = mkOpt path "/home/${config.user.name}/.openvpn/client.ovpn" "Path to the .ovpn configuration file on disk.";
   };
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      openvpn3
+      openvpn
     ];
+
+    services.openvpn.servers.${cfg.name} = {
+      autoStart = false;
+      config = ''
+        config ${cfg.configFile}
+
+        # Integrate with systemd-resolved for DNS management.
+        # This ensures VPN-pushed DNS servers are registered with resolved
+        # and cleaned up when the tunnel goes down.
+        script-security 2
+        setenv PATH /usr/bin:/bin:/usr/sbin:/sbin:${updateResolved}/libexec/openvpn
+        up ${updateResolved}/libexec/openvpn/update-systemd-resolved
+        up-restart
+        down ${updateResolved}/libexec/openvpn/update-systemd-resolved
+        down-pre
+      '';
+    };
   };
 }
